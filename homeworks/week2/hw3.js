775b5f9c1c5e6c827715da21ef30ff6956c0fa67
asd10042898 <81561777+asd10042898@users.noreply.github.com>
2021-04-25 16:12:20 +0800

    update

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,10 +1,8 @@
 function reverse(str) {
-	
 	var newstr = []
 	var a = str.length
 	for(var i=0; i<=str.length; i++){
 		newstr[i] = str[i+a-2*(i)]
-		
 	}
 	newstr = newstr.join('')
 	console.log(newstr)

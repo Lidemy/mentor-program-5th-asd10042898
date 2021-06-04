07e1dfe300d9c8b1a933c41cfb2b9a985cf732f1
asd10042898 <81561777+asd10042898@users.noreply.github.com>
2021-06-04 10:17:55 +0800

    hw1.js

--- homeworks/week4/hw1.js
+++ homeworks/week4/hw1.js
@@ -1,22 +1,22 @@
-const request = require('request');
+/* eslint-disable import/no-unresolved */
+const request = require('request')
 
-request('https://lidemy-book-store.herokuapp.com/books?_limit=10', function (err, response, body) {
-  //console.log(typeof body)可以查看資料是甚麼型態
-  if(err){
-    console.log('err', err)//判別資料是否正確
+request('https://lidemy-book-store.herokuapp.com/books?_limit=10', (err, response, body) => {
+  // console.log(typeof body)可以查看資料是甚麼型態
+  if (err) {
+    console.log('err', err)// 判別資料是否正確
     return
   }
 
   let books
-  try{
-    books = JSON.parse(body)//判別是否為json格式
-  }catch(err){
+  try {
+    books = JSON.parse(body)// 判別是否為json格式
+  } catch (err) {
     console.log(err)
     return
   }
-  
-  for(i=0; i<books.length; i++){
-    console.log(books[i].id + ' ' + books[i].name)
-  }
 
-});
+  for (let i = 0; i < books.length; i++) {
+    console.log(`${books[i].id} ${books[i].name}`)
+  }
+})
